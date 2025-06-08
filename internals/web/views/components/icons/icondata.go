63ee330ed9ff01efc57c713db98dc672fb31a787
diegorezm <dmourarezende@email.com>
2025-06-08 11:08:58 -0300

    icons

---
+++ internals/web/views/components/icons/icondata.go
@@ -0,0 +1,6 @@
+package icons
+
+var internalSvgData = map[string]string{
+	"bolt":  `<path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/><circle cx="12" cy="12" r="4"/>`,
+	"clock": `<circle cx="12" cy="12" r="10"/><polyline points="12 6 12 12 16 14"/>`,
+}
---
+++ internals/web/views/components/icons/icondefs.go
@@ -0,0 +1,4 @@
+package icons
+
+var Bolt = Icon("bolt")
+var Clock = Icon("clock")
