e2b0f190a7b5cdd72ddcc139b3829953692e131a
diegorezm <dmourarezende@email.com>
2025-06-09 15:52:07 -0300

    icons

--- internals/web/views/components/icons/icondata.go
+++ internals/web/views/components/icons/icondata.go
@@ -3,4 +3,5 @@ package icons
 var internalSvgData = map[string]string{
 	"bolt":  `<path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/><circle cx="12" cy="12" r="4"/>`,
 	"clock": `<circle cx="12" cy="12" r="10"/><polyline points="12 6 12 12 16 14"/>`,
+	"file":  `<path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/>`,
 }
--- internals/web/views/components/icons/icondefs.go
+++ internals/web/views/components/icons/icondefs.go
@@ -1,4 +1,5 @@
 package icons
 
 var Bolt = Icon("bolt")
+var File = Icon("file")
 var Clock = Icon("clock")
