a9660d9084d31cab6138ce375bc3f8b19e9a46f7
diegorezm <dmourarezende@email.com>
2025-06-10 15:27:58 -0300

    web

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -12,4 +12,7 @@ func RegisterAll(mux *http.ServeMux, s *store.Queries) {
 
 	pagesHandler := NewPagesHandler(s)
 	pagesHandler.RegisterAll(mux)
+
+	sectionsHandler := NewSectionHandler(s)
+	sectionsHandler.RegisterAll(mux)
 }
